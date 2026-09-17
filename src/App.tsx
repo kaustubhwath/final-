42af2541e15eb705181d8b526f52f2e77fafc905
kaustubh <kaustubhwath16@gmail.com>
2026-09-17 10:03:10 +0530

    Create initial App component with basic layout

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>App Ready</h1>
+      <p>Your project files are set up and ready to deploy!</p>
+    </div>
+  )
+}
